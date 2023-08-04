c5138e527b9537fed0cfa2d8a65729c1558e75bd
pm6422 <pm6422@126.com>
2023-08-04 20:09:40 +0800

    refactor

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-./mvnw -s /Users/louislau/Workspace/infinity-rpc/.mvn/settings.xml -DskipTests clean javadoc:jar deploy -P release
+./mvnw -s /Users/louislau/Workspace/luix-rpc/.mvn/settings.xml -DskipTests clean javadoc:jar deploy -P release
