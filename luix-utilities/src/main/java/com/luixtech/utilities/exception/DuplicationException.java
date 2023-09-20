01022aedb6e06b69c31c1db6c79d8233d27f21ad
pm6422 <pm6422@126.com>
2023-09-20 19:58:18 +0800

    refactor

--- luix-utilities/src/main/java/com/luixtech/utilities/exception/DuplicationException.java
+++ luix-utilities/src/main/java/com/luixtech/utilities/exception/DuplicationException.java
@@ -3,6 +3,7 @@ package com.luixtech.utilities.exception;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Iterator;
 import java.util.Map;
 
 @Data
@@ -13,7 +14,21 @@ public class DuplicationException extends RuntimeException {
     private final        Map<String, Object> fieldMap;
 
     public DuplicationException(Map<String, Object> fieldMap) {
-        super("Found duplicated data!");
         this.fieldMap = fieldMap;
+    }
+
+    @Override
+    public String getMessage() {
+        StringBuffer sb = new StringBuffer("Found duplicated data for { ");
+        Iterator<Map.Entry<String, Object>> iterator = fieldMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Object> entry = iterator.next();
+            sb.append(entry.getKey()).append(": ").append(entry.getValue());
+            if (iterator.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append(" }");
+        return sb.toString();
     }
 }
