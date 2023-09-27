f3192646f9615694c729ee814757db823587e1d6
louis <pm6422@126.com>
2023-09-27 17:08:51 +0800

    re

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 export GPG_TTY=$(tty)
 
+# For Mac env
 ./mvnw -s /Users/louislau/Workspace/tech-docs/maven/settings.xml -DskipTests clean javadoc:jar deploy -P release
 
-# Ubuntu env
+# For Ubuntu env
 #./mvnw -s /home/louis/Workspace/tech-docs/maven/settings.xml -DskipTests clean javadoc:jar deploy -P release
