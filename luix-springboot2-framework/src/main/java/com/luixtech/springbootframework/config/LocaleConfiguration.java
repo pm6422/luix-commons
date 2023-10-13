fd2afdd36bb0af3590119636f233a44d3887dc67
louis <pm6422@126.com>
2023-10-13 09:18:23 +0800

    re

--- luix-springboot2-framework/src/main/java/com/luixtech/springbootframework/config/LocaleConfiguration.java
+++ luix-springboot2-framework/src/main/java/com/luixtech/springbootframework/config/LocaleConfiguration.java
@@ -1,15 +1,15 @@
 package com.luixtech.springbootframework.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * Default LocaleResolver is {@link AcceptHeaderLocaleResolver}
- * Refer to {@link WebMvcConfigurationSupport#localeResolver()}
+ * Refer to {@link WebMvcAutoConfiguration.EnableWebMvcConfiguration#localeResolver()}
  */
 @Configuration
 public class LocaleConfiguration implements WebMvcConfigurer {
