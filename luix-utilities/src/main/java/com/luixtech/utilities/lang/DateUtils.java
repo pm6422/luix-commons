7bc8cc8aa297ba9aabe9d8dd8093b11d5992a8d1
pm6422 <pm6422@126.com>
2024-05-12 14:27:27 +0800

    refactor

---
+++ luix-utilities/src/main/java/com/luixtech/utilities/lang/DateUtils.java
@@ -0,0 +1,7 @@
+package com.luixtech.utilities.lang;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+
+public class DateUtils {
+    public static final FastDateFormat FILE_DATETIME_FORMAT = FastDateFormat.getInstance("yyyyMMdd-HHmmss");
+}
