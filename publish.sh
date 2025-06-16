41eb9e042e95115ad1a3d5fc2fec8c207edcda02
pm6422 <pm6422@126.com>
2025-06-16 22:59:29 +0200

    refactor

--- publish.sh
+++ publish.sh
@@ -3,5 +3,8 @@ export GPG_TTY=$(tty)
 # For Mac env
 ./mvnw -s /Users/louislau/Workspace/tech-docs/maven/settings.xml -DskipTests clean javadoc:jar deploy -P release
 
+# For Mac env with debug info
+#./mvnw -s /Users/louislau/Workspace/tech-docs/maven/settings.xml -DskipTests clean javadoc:jar deploy -P release -X
+
 # For Ubuntu env
 #./mvnw -s /home/louis/Workspace/tech-docs/maven/settings.xml -DskipTests clean javadoc:jar deploy -P release
